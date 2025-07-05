8da307b05e02cbd11230190fd0bd62adefb05840
KartikSisodia217 <sisodiakartik21@gmail.com>
2025-07-05 13:09:50 +0530

    my commit on 05/07/2025

--- first_Java_Project/src/Pattern.java
+++ first_Java_Project/src/Pattern.java
@@ -1,5 +1,5 @@
-public class Pattern {
-    public static void main (String args[]){
+class designs{
+    public String pattern1(){
         int i=1;
         int j=1;
         for (i=1; i<=4; i++) {
@@ -8,5 +8,101 @@ public class Pattern {
             }
             System.out.println("");
         }
+        return "pattern 1 printed";
+    }
+    public String pattern2(){
+        for (int i=1; i<=4;i++){
+            for (int j=(4-i);j>=0;j--){
+                System.out.print(" ");
+            }
+            for (int j =1; j<=i; j++){
+                System.out.print("*");
+            }
+            for (int j =1; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        return "pattern 2 printed";
+    }
+    public String pattern3(){
+        for (int i=1; i<=4;i++){
+            for (int j=(3-i);j>=0;j--){
+                System.out.print(" ");
+            }
+            for (int j =1; j<=i; j++){
+                System.out.print("*");
+            }
+            for (int j =1; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for(int i=1; i<4; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j=(3-i);j>=0;j--){
+                System.out.print("*");
+            }
+            for(int j=(2-i);j>=0;j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        return "pattern 3 printed";
+    }
+    public String pattern4(){
+        for(int i=1; i<4; i++){
+            for(int j=1; j<i; j++){
+                System.out.print(" ");
+            }
+            for(int j=(4-i);j>=0;j--){
+                System.out.print("*");
+            }
+            for(int j=(3-i);j>=0;j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int i=1; i<=4;i++){
+            for (int j=(3-i);j>=0;j--){
+                System.out.print(" ");
+            }
+            for (int j =1; j<=i; j++){
+                System.out.print("*");
+            }
+            for (int j =1; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        return "pattern 4 printed";
+    }
+    public String pattern5(){
+        for (int i=1; i<=4;i++){
+            for (int j=1; j<=7; j++){
+                if(i==1 || j==1 || i==4 || j==7) {
+                    System.out.print("*");
+                }
+                else{
+                        System.out.print(" ");
+                    }
+            }
+            System.out.println("");
+        }
+        return "pattern 5 printed";
+    }
+
+}
+
+public class Pattern {
+    public static void main (String args[]){
+        designs des= new designs();
+        System.out.println(des.pattern1());
+        System.out.println(des.pattern2());
+        System.out.println(des.pattern3());
+        System.out.println(des.pattern4());
+        System.out.println(des.pattern5());
     }
 }
